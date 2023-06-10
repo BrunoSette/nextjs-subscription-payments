bbdd37c264c856377d1ee4a4ac116e0724e99fee
Bruno Sette <brunosette@gmail.com>
2023-06-10 09:06:03 -0400

    Add footer

--- components/ui/Footer/Footer.tsx
+++ components/ui/Footer/Footer.tsx
@@ -1,110 +1,79 @@
-import Link from 'next/link';
-
 import Logo from '@/components/icons/Logo';
-import GitHub from '@/components/icons/GitHub';
+import Link from 'next/link';
 
 export default function Footer() {
   return (
     <footer className="mx-auto max-w-[1920px] px-6 bg-zinc-900">
-      <div className="grid grid-cols-1 gap-8 py-12 text-white transition-colors duration-150 border-b lg:grid-cols-12 border-zinc-600 bg-zinc-900">
-        <div className="col-span-1 lg:col-span-2">
+      <div className="grid grid-cols-2 md:grid-cols-1 lg:grid-cols-12 gap-8 border-b border-zinc-600 py-12 text-white transition-colors duration-150 bg-zinc-900">
+        <div className="col-span-2 lg:col-span-2">
           <Link
             href="/"
-            className="flex items-center flex-initial font-bold md:mr-24"
+            className="flex flex-initial items-center font-bold md:mr-24"
           >
-            <span className="mr-2 border rounded-full border-zinc-700">
+            <span className="rounded-full border border-zinc-700 mr-2">
               <Logo />
             </span>
-            <span>ACME</span>
+            <span>My Job Upgrade</span>
           </Link>
         </div>
         <div className="col-span-1 lg:col-span-2">
-          <ul className="flex flex-col flex-initial md:flex-1">
+          <ul className="flex flex-initial flex-col md:flex-1">
             <li className="py-3 md:py-0 md:pb-4">
               <Link
                 href="/"
-                className="text-white transition duration-150 ease-in-out hover:text-zinc-200"
+                className="text-white hover:text-zinc-200 transition ease-in-out duration-150"
               >
                 Home
               </Link>
             </li>
             <li className="py-3 md:py-0 md:pb-4">
               <Link
-                href="/"
-                className="text-white transition duration-150 ease-in-out hover:text-zinc-200"
+                href="/account"
+                className="text-white hover:text-zinc-200 transition ease-in-out duration-150"
               >
-                About
+                Account
               </Link>
             </li>
             <li className="py-3 md:py-0 md:pb-4">
               <Link
-                href="/"
-                className="text-white transition duration-150 ease-in-out hover:text-zinc-200"
+                href="/resume"
+                className="text-white hover:text-zinc-200 transition ease-in-out duration-150"
               >
-                Careers
-              </Link>
-            </li>
-            <li className="py-3 md:py-0 md:pb-4">
-              <Link
-                href="/"
-                className="text-white transition duration-150 ease-in-out hover:text-zinc-200"
-              >
-                Blog
+                Resume
               </Link>
             </li>
           </ul>
         </div>
         <div className="col-span-1 lg:col-span-2">
-          <ul className="flex flex-col flex-initial md:flex-1">
+          <ul className="flex flex-initial flex-col md:flex-1">
             <li className="py-3 md:py-0 md:pb-4">
-              <p className="font-bold text-white transition duration-150 ease-in-out hover:text-zinc-200">
+              <p className="text-white font-bold hover:text-zinc-200 transition ease-in-out duration-150">
                 LEGAL
               </p>
             </li>
             <li className="py-3 md:py-0 md:pb-4">
               <Link
-                href="/"
-                className="text-white transition duration-150 ease-in-out hover:text-zinc-200"
+                href="/privacypolicy"
+                className="text-white hover:text-zinc-200 transition ease-in-out duration-150"
               >
                 Privacy Policy
               </Link>
             </li>
             <li className="py-3 md:py-0 md:pb-4">
               <Link
-                href="/"
-                className="text-white transition duration-150 ease-in-out hover:text-zinc-200"
+                href="/terms"
+                className="text-white hover:text-zinc-200 transition ease-in-out duration-150"
               >
                 Terms of Use
               </Link>
             </li>
           </ul>
         </div>
-        <div className="flex items-start col-span-1 text-white lg:col-span-6 lg:justify-end">
-          <div className="flex items-center h-10 space-x-6">
-            <a
-              aria-label="Github Repository"
-              href="https://github.com/vercel/nextjs-subscription-payments"
-            >
-              <GitHub />
-            </a>
-          </div>
-        </div>
+        <div className="col-span-2 lg:col-span-6 flex items-start lg:justify-end text-white"></div>
       </div>
-      <div className="flex flex-col items-center justify-between py-12 space-y-4 md:flex-row bg-zinc-900">
+      <div className="py-12 flex flex-col md:flex-row justify-between items-center space-y-4 bg-zinc-900">
         <div>
-          <span>
-            &copy; {new Date().getFullYear()} ACME, Inc. All rights reserved.
-          </span>
-        </div>
-        <div className="flex items-center">
-          <span className="text-white">Crafted by</span>
-          <a href="https://vercel.com" aria-label="Vercel.com Link">
-            <img
-              src="/vercel.svg"
-              alt="Vercel.com Logo"
-              className="inline-block h-6 ml-4 text-white"
-            />
-          </a>
+          <span>&copy; 2023 My Job Upgrade. All rights reserved.</span>
         </div>
       </div>
     </footer>
